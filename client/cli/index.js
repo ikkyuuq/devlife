15545cb780f4835cbbe33dc1771fe50819eec5e9
ikkyuuq <kittipongprasompong@gmail.com>
2024-10-05 03:35:30 +0700

    💫 feat: submit task with node process and UI with node-cli/logger and tasuku

--- client/cli/index.js
+++ client/cli/index.js
@@ -1,6 +1,13 @@
 import { Command } from "commander";
 import fs from "fs";
 import inquirer from "inquirer";
+import { spawn } from "node:child_process";
+import tasuku from "tasuku";
+import { performance } from "perf_hooks";
+import { Logger, Spinner } from "@node-cli/logger";
+
+const logger = new Logger();
+const spinner = new Spinner();
 
 const app = new Command();
 
@@ -14,8 +21,6 @@ const devlife = fs.existsSync(
     )
   : {};
 
-app.name("devlife").description("CLI tools for devlife");
-
 app
   .command("auth")
   .description("Authenticate with devlife")
@@ -24,15 +29,38 @@ app
   .option("-o, --oauth <email>", "Authenticate with GitHub OAuth")
   .action(async (options) => {
     if (options.signout) {
+      if (!devlife.token) {
+        spinner.start();
+        spinner.text = "";
+        spinner.stop("Already signed out", Spinner.SUCCESS);
+        return;
+      }
+
       const homeDir = process.env.HOME || process.env.USERPROFILE;
       const devlifeDir = `${homeDir}/.devlife`;
       const configPath = `${devlifeDir}/config.json`;
+
       // Replace the file with an empry object
-      fs.writeFileSync(configPath, JSON.stringify({}, null, 2));
-      console.log("Signed out of devlife");
-      return;
+      try {
+        fs.writeFileSync(configPath, JSON.stringify({}, null, 2));
+        spinner.start();
+        spinner.text = "Signing out of devlife...";
+        spinner.stop("Signed out of devlife");
+        return;
+      } catch (e) {
+        spinner.stop("Error signing out of devlife", Spinner.ERROR);
+        return;
+      }
     }
+
     if (options.email) {
+      if (devlife.token) {
+        spinner.start();
+        spinner.text = "";
+        spinner.stop("Already signed in", Spinner.SUCCESS);
+        return;
+      }
+
       const email = options.email;
       const { password } = await inquirer.prompt([
         {
@@ -42,6 +70,7 @@ app
           mask: "*",
         },
       ]);
+
       async function emailAuth() {
         const resp = await fetch("http://localhost:3000/cli-signin", {
           method: "POST",
@@ -56,22 +85,29 @@ app
         });
         return resp;
       }
+
+      spinner.start();
+      spinner.text = "Signing in...";
+
       const resp = await emailAuth();
       if (resp.status === 401) {
         resp.json().then((data) => {
-          console.log(data.error);
+          spinner.stop(`${data.error}`, Spinner.ERROR);
         });
         return;
       }
+
       const data = await resp.json();
       const homeDir = process.env.HOME || process.env.USERPROFILE;
       const devlifeDir = `${homeDir}/.devlife`;
       const configPath = `${devlifeDir}/config.json`;
+
       // Create the directory if it doesn't exist
       fs.mkdirSync(devlifeDir, { recursive: true });
       // Write the token to the config file
       fs.writeFileSync(configPath, JSON.stringify(data, null, 2));
-      console.log("Authenticated with devlife!");
+
+      spinner.stop("Singned in to devlife", Spinner.SUCCESS);
     } else if (options.oauth) {
       const email = options.oauth;
       async function githubAuth() {
@@ -108,20 +144,174 @@ app
   .command("list")
   .alias("ls")
   .description("List tasks from devlife")
+  // TODO: implement fetch tasks by status (todo, done)
   .option("-f, --filter <filter>", "Filter tasks by status (done, todo)")
   .option("-t, --tags <tags...>", "Filter items by type")
   .action(async (options) => {
     const resp = await listAllTasks();
     if (resp.status === 401) {
-      console.log("Please authenticate with `devlife auth`");
+      logger.error("Please authenticate with `devlife auth`");
       return;
     }
     const data = await resp.json();
-    console.log(data);
+    const cliTask = await data.tasks.map((task) => {
+      return {
+        id: task.id,
+        title: task.title,
+        objective: task.objective,
+      };
+    });
+
+    // TODO: limit the number of tasks to display
+    logger.printBox(
+      [
+        cliTask
+          .map((task) => {
+            const id = `\x1b[1m\x1b[33m[${task.id}]\x1b[0m `;
+            const title = `\x1b[1m${task.title}\x1b[0m`;
+            const objective = `\x1b[2m${task.objective}\x1b[0m`;
+            return `${id}\n${title}\n${objective}`;
+          })
+          .join("\n\n"),
+      ],
+      {
+        align: "left",
+        newLineBefore: false,
+        newLineAfter: false,
+      },
+    );
   });
 
-app.command("submit").action(() => {
-  console.log("submit");
-});
+// Retry logic to handle multiple-line input failure and attempt single-line
+const runTestWithRetry = async (file, input, setTitle) => {
+  // First, try with multiple-line input
+  setTitle("Running test...");
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  let result = await testRunner(file, input);
+
+  if (result.error || result.code !== 0) {
+    // Prepare single-line input
+    const singleLineInput = input.map(String).join(" ");
+
+    setTitle("Retrying test...");
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    // Retry with single-line input
+    result = await testRunner(file, [singleLineInput]);
+  }
+
+  return result;
+};
+
+const testRunner = async (file, input) => {
+  return new Promise((resolve, reject) => {
+    let output = "";
+    let error = "";
+    let startTime, endTime;
+    switch (true) {
+      case file.endsWith(".py"):
+        startTime = performance.now();
+        const child = spawn("python", [file]);
+        child.stdout.on("data", (data) => {
+          output += data.toString();
+        });
+
+        child.stderr.on("data", (data) => {
+          error += data.toString();
+        });
+
+        child.on("close", (code) => {
+          endTime = performance.now();
+          const timeTaken = endTime - startTime;
+          resolve({
+            output: output.trim(),
+            error: error.trim(),
+            code,
+            timeTaken,
+          });
+        });
+
+        try {
+          input.map((n) => {
+            child.stdin.write(`${n}\n`);
+          });
+        } catch (e) {
+          throw new Error(e);
+        }
+
+        child.stdin.end();
+        break;
+      default:
+        reject(new Error(`Unsupported file type: ${file}`));
+    }
+  });
+};
+
+const runTask = async (taskid, file) => {
+  const resp = await fetch(`http://localhost:3000/task/${taskid}`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${devlife.token}`,
+    },
+  });
+  if (resp.status === 401) {
+    logger.error("Please authenticate with `devlife auth`");
+    return;
+  }
+  const data = await resp.json();
+  const tests = data.task.tests;
+  const tests_json = JSON.parse(tests);
+  const testCount = tests_json.data.length;
+
+  spinner.start();
+  spinner.text = "Running all tests...";
+
+  const tasks = await Promise.all(
+    tests_json.data.map(async (test, index) => {
+      return tasuku(
+        `Test ${index + 1}`,
+        async ({ setTitle, setError, setOutput, setStatus }) => {
+          const { output, error, timeTaken } = await runTestWithRetry(
+            file,
+            test.input,
+            setTitle,
+          );
+          if (error) {
+            throw new Error(error);
+          }
+
+          if (test.expected === output) {
+            setStatus(`⚡${timeTaken.toFixed(2)} ms`);
+            setTitle(`Test ${index + 1}`);
+            return "Passed";
+          } else {
+            setStatus(`💥${timeTaken.toFixed(2)} ms`);
+            setError();
+            setOutput(
+              `${test.input.length > 0 ? `Inputs: ${test.input}\n` : ""}Expected: ${test.expected}\nActual: ${output}`,
+            );
+            return "Failed";
+          }
+        },
+      );
+    }),
+  );
+
+  if (tasks.some((task) => task.result === "Failed")) {
+    spinner.stop("Some tests failed", Spinner.ERROR);
+    return;
+  }
+  spinner.stop(`All ${testCount} tests passed`, Spinner.SUCCESS);
+};
+app
+  .command("submit")
+  .arguments("<taskid>")
+  .arguments("<solution>")
+  .action(async (taskid, solution) => {
+    await runTask(taskid, solution);
+  });
+
+app.name("devlife").description("CLI tools for devlife").version("0.0.1");
 
 app.parse(process.argv);
